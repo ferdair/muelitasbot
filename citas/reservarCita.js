1a0e4e708b0d894eb80afdfa5bff5554cca69bc5
David Calapaqui <hdcalapaqui@espe.edu.ec>
2021-01-27 21:06:20 -0500

    Rango de fechas arreglado x6

--- citas/reservarCita.js
+++ citas/reservarCita.js
@@ -66,7 +66,7 @@ function getHoraDisponible(dia) {
 
     let minDia = new Date(new Date(dia).setHours(dia.getHours() - 5)); //desde las 00 horas
     console.log('Consultar desde: ', minDia);
-    let maxDia = new Date(new Date(dia).setHours(dia.getHours() - 5)); //hasta las 22 hrs
+    let maxDia = new Date(new Date(dia).setHours(minDia.getHours() + 22)); //hasta las 22 hrs
     let fechaHoraUltimaCita;
     console.log('Consultar hasta: ', maxDia);
 
