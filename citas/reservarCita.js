d6b9369dba36215049920589f7dcb087ed28f06b
David Calapaqui <hdcalapaqui@espe.edu.ec>
2021-03-03 23:56:04 -0500

    Cancela cita y quick replies para dar el motivo

--- citas/reservarCita.js
+++ citas/reservarCita.js
@@ -363,18 +363,7 @@ function getCitaACancelar(idUser) {
                     citasUsuario.map((event, i) => {
                         console.log(` ID: ${event.id} - Título: ${event.summary} - Inicio ${(event.start.dateTime)}`);
                     });
-                    //si hay mas de una cita, envio la primera que es la mas cercana a la actualidad
-                    /*if (citasUsuario.length > 1) {
-                        resp.hayCita = true;
-                        resp.text = `Tienes una cita agendada el día ${moment(citasUsuario[citasUsuario.length -1].start.dateTime || citasUsuario[citasUsuario.length -1].start.date ).format('LLLL')}`
-                        resp.id = citasUsuario[citasUsuario.length - 1].id;
-                        resolve(resp);
-                    } else {
-                        resp.hayCita = true;
-                        resp.text = `Tienes una cita agendada el día ${moment(citasUsuario[0].start.dateTime || citasUsuario[0].start.date ).format('LLLL')}`
-                        resp.id = citasUsuario[0].id;
-                        resolve(resp);
-                    }*/
+
                     resp.hayCita = true;
                     resp.text = `Tienes una cita agendada el día ${moment(citasUsuario[0].start.dateTime || citasUsuario[0].start.date ).format('LLLL')}`
                     resp.id = citasUsuario[0].id;
