09c518d2576bc883e26902f56053f7c0b573b0a3
David Calapaqui <hdcalapaqui@espe.edu.ec>
2021-03-10 20:52:46 -0500

    Responde que no hay chance si ya es mas tarde del fin del horario de atenciox9

--- facebook/facebookBot.js
+++ facebook/facebookBot.js
@@ -234,7 +234,7 @@ async function handleDialogFlowAction(
             let diaAConsultar = new Date(Date.parse(strFecha.split('T')[0] + 'T' + strFecha.split('T')[1].split('-')[0] + '-05:00'));
             console.log('Dia a consultar: ', diaAConsultar);
             console.log(`Resp. metdo Horario: ${dias.dentroDeHorario()}`);
-            if (dias.dentroDeHorario === 1) {
+            if (dias.dentroDeHorario() === 1) {
 
                 sendTextMessage(sender, 'Ya no te podemos agendar una cita hoy');
                 return;
